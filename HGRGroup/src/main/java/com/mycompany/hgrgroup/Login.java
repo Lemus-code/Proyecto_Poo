dc166ab80a7bf21933ff4a978cded78001ce7abd
urielesau <urielesauperezmorales@gmail.com>
2023-10-23 10:51:08 -0600

    Clase calendario

--- HGRGroup/src/main/java/com/mycompany/hgrgroup/Login.java
+++ HGRGroup/src/main/java/com/mycompany/hgrgroup/Login.java
@@ -140,7 +140,7 @@ public class Login extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelDinamicoLayout.createSequentialGroup()
                         .addComponent(bntIniciar, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(126, 126, 126)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 141, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         panelDinamicoLayout.setVerticalGroup(
